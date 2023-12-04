5d9c3241668af8612d02e08e189980bd891e222d
Никита Александрович Плеханов <__@microbe.local>
2023-12-04 16:53:57 +0400

    04.12.2023

--- Preprocessing.R
+++ Preprocessing.R
@@ -49,7 +49,7 @@ sber_un <- sber_un %>%
 anyNA(sber_un)
       
 # DomClick ----------------------------------------------------------------
-# Domclick_credit:
+### Domclick_credit:
 
 # Создаем функцию для чтения и обработки данных:
 domclck_prep <- function(dat){
@@ -107,3 +107,79 @@ domclck_un <- rbind(domclck_1,
 domclck_un <- domclck_un %>% 
   arrange(Регион)
 
+### Domclick_hypothec:
+
+# Создаем функцию для чтения и обработки данных:
+domclck_hyp_prep <- function(dat){
+  dat$Месяц <- "X"
+  dat$Год <- 2020
+  dat <- dat %>% 
+    select(`Регион`, `Год`, `Месяц`, `Всего ипотечных сделок` : `Доля сделок, вторичка`)
+  dat <- dat %>% 
+    mutate(`Доля сделок, первичка` = as.numeric(str_replace(`Доля сделок, первичка`, "%", "")) / 100)
+  dat <- dat %>% 
+    mutate(`Доля сделок, вторичка` = as.numeric(str_replace(`Доля сделок, вторичка`, "%", "")) / 100)
+  dat$`Всего ипотечных сделок` <- factor(dat$`Всего ипотечных сделок`,
+                                          ordered = TRUE,
+                                          levels = c("10 - 50",
+                                                     "50 - 100",
+                                                     "100 - 500",
+                                                     "500 - 1 000",
+                                                     "> 1 000"))
+  dat <- drop_na(dat)
+  return(dat)
+}
+
+# Создаем вектор с именами:
+names2_ls = c()
+for(i in seq(1:12)){
+  x <- paste("dmclck_hyp", i, sep = "_")
+  names2_ls[i] <- x
+}
+
+# Создаем вектор с месяцами:
+month <- as.character(sber_un$Месяц[1:12])
+
+# Читаем, обрабатываем и записываем файлы:
+ls2 = list.files("C:/R/Skillbox/R_diplom/Рейтинг регионов по количеству ипотечных сделок/")
+for(i in seq(1, 12)){
+  dat <- read_xlsx(paste0("C:/R/Skillbox/R_diplom/Рейтинг регионов по количеству ипотечных сделок/", ls2[i]))
+  dat <- domclck_hyp_prep(dat)
+  dat$Месяц <- month[i]
+  assign(paste("domclck_hyp", i, sep = "_"), dat)
+}
+
+domclck_hyp_un <- rbind(domclck_hyp_1, 
+                    domclck_hyp_2, 
+                    domclck_hyp_3,
+                    domclck_hyp_4,
+                    domclck_hyp_5,
+                    domclck_hyp_6,
+                    domclck_hyp_7,
+                    domclck_hyp_8,
+                    domclck_hyp_9,
+                    domclck_hyp_10,
+                    domclck_hyp_11,
+                    domclck_hyp_12)
+
+domclck_hyp_un <- domclck_hyp_un %>% 
+  arrange(Регион)
+
+domclck_both <- left_join(domclck_un, domclck_hyp_un)
+
+
+# Rosstat -----------------------------------------------------------------
+
+rosstat_zp <- read_xlsx("Rosstat_Заработная плата.xlsx", sheet = "с 2019", skip = 2)
+rosstat_zp <-  rosstat_zp %>% 
+  select(...1, январь...14 : декабрь...25)
+colnames(rosstat_zp) <- c("Регион", month)
+rosstat_zp <- rosstat_zp[-c(24, 69), ]
+rosstat_zp <- pivot_longer(rosstat_zp, cols = "Январь" : "Декабрь", names_to = "Месяц", values_to = "Среднемесячная заработная плата")
+rosstat_zp <- rosstat_zp %>% 
+  filter(Регион != "Российская Федерация")
+rosstat_zp <- rosstat_zp %>% 
+  mutate(`Квартал` = case_when(`Месяц` %in% month[1:3] ~ "Квартал 1",
+                               `Месяц` %in% month[4:6] ~ "Квартал 2",
+                               `Месяц` %in% month[7:9] ~ "Квартал 3",
+                               `Месяц` %in% month[10:12] ~ "Квартал 4"), .after = `Месяц`)
