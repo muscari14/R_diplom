ea1ffc68bb24f1fc28c234e8793b3d7343ce11e9
Никита Александрович Плеханов <__@microbe.local>
2023-12-04 14:46:27 +0400

    04.12.2023

--- Preprocessing.R
+++ Preprocessing.R
@@ -49,20 +49,61 @@ sber_un <- sber_un %>%
 anyNA(sber_un)
       
 # DomClick ----------------------------------------------------------------
+# Domclick_credit:
 
-domclck1 <- read_xlsx("Рейтинг регионов по количеству заявок на кредит/январь 2020.xlsx")
-domclck1$Месяц <- "Январь"
-domclck1$Год <- 2020
-domclck1 <- domclck1 %>% 
-  select(`Регион`, `Год`, `Месяц`, `Всего одобренных заявок` : `Доля заявок в офисе банка`)
-domclck1 <- domclck1 %>% 
-  mutate(`Доля онлайн-заявок` = as.numeric(str_replace(`Доля онлайн-заявок`, "%", "")) / 100)
-domclck1 <- domclck1 %>% 
-  mutate(`Доля заявок в офисе банка` = as.numeric(str_replace(`Доля заявок в офисе банка`, "%", "")) / 100)
-domclck1$`Всего одобренных заявок` <- factor(domclck1$`Всего одобренных заявок`,
-                                             ordered = TRUE,
-                                             levels = c("100 - 500",
-                                                        "500 - 1 000",
-                                                        "1 000 - 5 000",
-                                                        "5 000 - 10 000"))
-domclck1$`Всего одобренных заявок`
+# Создаем функцию для чтения и обработки данных:
+domclck_prep <- function(dat){
+  dat$Месяц <- "X"
+  dat$Год <- 2020
+  dat <- dat %>% 
+    select(`Регион`, `Год`, `Месяц`, `Всего одобренных заявок` : `Доля заявок в офисе банка`)
+  dat <- dat %>% 
+    mutate(`Доля онлайн-заявок` = as.numeric(str_replace(`Доля онлайн-заявок`, "%", "")) / 100)
+  dat <- dat %>% 
+    mutate(`Доля заявок в офисе банка` = as.numeric(str_replace(`Доля заявок в офисе банка`, "%", "")) / 100)
+  dat$`Всего одобренных заявок` <- factor(dat$`Всего одобренных заявок`,
+                                               ordered = TRUE,
+                                               levels = c("100 - 500",
+                                                          "500 - 1 000",
+                                                          "1 000 - 5 000",
+                                                          "5 000 - 10 000",
+                                                          "> 10 000"))
+  dat <- drop_na(dat)
+  return(dat)
+}
+
+# Создаем вектор с именами:
+names_ls = c()
+for(i in seq(1:12)){
+  x <- paste("dmclck_cred", i, sep = "_")
+  names_ls[i] <- x
+}
+
+# Создаем вектор с месяцами:
+month <- as.character(sber_un$Месяц[1:12])
+
+# Читаем, обрабатываем и записываем файлы:
+ls = list.files("C:/R/Skillbox/R_diplom/Рейтинг регионов по количеству заявок на кредит/")
+for(i in seq(1, 12)){
+  dat <- read_xlsx(paste0("C:/R/Skillbox/R_diplom/Рейтинг регионов по количеству заявок на кредит/", ls[i]))
+  dat <- domclck_prep(dat)
+  dat$Месяц <- month[i]
+  assign(paste("domclck", i, sep = "_"), dat)
+}
+
+domclck_un <- rbind(domclck_1, 
+                   domclck_2, 
+                   domclck_3,
+                   domclck_4,
+                   domclck_5,
+                   domclck_6,
+                   domclck_7,
+                   domclck_8,
+                   domclck_9,
+                   domclck_10,
+                   domclck_11,
+                   domclck_12)
+
+domclck_un <- domclck_un %>% 
+  arrange(Регион)
+
