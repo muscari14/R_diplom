5a4e6eb73722ed0227bc6e1e16b77ce841d50855
Никита Александрович Плеханов <__@microbe.local>
2024-08-12 11:20:56 +0400

    12.08.2024

--- GoodPractice.R
+++ GoodPractice.R
@@ -1,5 +1,10 @@
 library(shiny)
+library(tidyverse)
+library(geojsonio)
+library(leaflet)
 library(RColorBrewer)
+library(psych)
+library(htmltools)
 
 rus <- geojson_read("geojson_rus_fin2.geojson", what = "sp")
 
@@ -16,9 +21,12 @@ server <- function(input, output, session) {
   })
   
   colorpal <-  reactive({
-    colorBin(input$color,  rus@data[[input$value]], bins = 4, pretty = TRUE)
+    colorBin(input$color,  rus@data[[input$value]], bins = 4, pretty = TRUE, na.color = "wheat")
     })
 
+  labels <- reactive({paste("</strong>", rus@data$name, "</strong><br>", input$value, ":", round(rus@data[[input$value]], 2)) %>% 
+      lapply(htmltools::HTML)
+  })
   
   output$map <- renderLeaflet({
     leaflet(rus) %>% 
@@ -28,8 +36,17 @@ server <- function(input, output, session) {
   observe({
     pal <- colorpal()
     leafletProxy("map", data = rus) %>% 
-      clearShapes() %>% 
-      addPolygons(fillColor = ~pal(rus[[actualvalue()]]))
+      clearShapes() %>%
+      clearControls() %>% 
+      addPolygons(fillColor = ~pal(rus[[actualvalue()]]), 
+                  weight = 0.8, 
+                  opacity = 1, 
+                  color = "steelblue", 
+                  dashArray = "3", 
+                  fillOpacity = 0.7,  
+                  highlightOptions = highlightOptions(weight = 1, color = "coral", fillOpacity = 0.85, dashArray = "", bringToFront = TRUE),  
+                  label = ~labels()) %>% 
+      addLegend(pal = pal, values = rus@data[[input$value]], opacity = 0.7, title = NULL, position = "bottomright")
   })
   
 }
@@ -43,4 +60,4 @@ rus@data$salary
 rus@data["salary"]
 
 
-
+?addPolygons()
