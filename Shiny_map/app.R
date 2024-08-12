94e8303faa9d06a06e1d0272a6548fc4751caf0d
Никита Александрович Плеханов <__@microbe.local>
2024-08-12 12:46:28 +0000

    12.02.2024

--- Shiny_map/app.R
+++ Shiny_map/app.R
@@ -1,17 +1,21 @@
+#Необходимые библиотеки:
 library(shiny)
 library(tidyverse)
 library(geojsonio)
 library(leaflet)
 library(RColorBrewer)
-library(psych)
 library(htmltools)
 
-rus <- geojson_read("geojson_rus_fin2.geojson", what = "sp")
+#Чтение данных:
+rus <- geojson_read("rus_fin.geojson", what = "sp")
+gsub("\\.", " ", colnames(rus@data[6:8]))
+colnames(rus@data)[6:8] <- gsub("\\.", " ", colnames(rus@data)[6:8])
 
+#Пользовательский интерфейс
 ui <- fluidPage(
   leafletOutput("map"),
-  selectInput("value", "Показатель", choices = colnames(rus@data[6:8])),
-  selectInput("color", "Схема", choices = rownames(subset(brewer.pal.info, category == "seq")))
+  selectInput("color", "Выберите палитру:", choices = rownames(subset(brewer.pal.info, category == "seq"))),
+  selectInput("value", "Выберите показатель:", choices = colnames(rus@data)[6:8])
 )
 
 server <- function(input, output, session) {
@@ -22,8 +26,8 @@ server <- function(input, output, session) {
   
   colorpal <-  reactive({
     colorBin(input$color,  rus@data[[input$value]], bins = 4, pretty = TRUE, na.color = "wheat")
-    })
-
+  })
+  
   labels <- reactive({paste("</strong>", rus@data$name, "</strong><br>", input$value, ":", round(rus@data[[input$value]], 2)) %>% 
       lapply(htmltools::HTML)
   })
@@ -31,8 +35,8 @@ server <- function(input, output, session) {
   output$map <- renderLeaflet({
     leaflet(rus) %>% 
       addTiles() %>% 
-      setView(lng = 100, lat = 66, zoom = 2)
-    })
+      setView(lng = 100, lat = 60, zoom = 2)
+  })
   
   observe({
     pal <- colorpal()
@@ -44,21 +48,12 @@ server <- function(input, output, session) {
                   opacity = 1, 
                   color = "steelblue", 
                   dashArray = "3", 
-                  fillOpacity = 0.7,  
+                  fillOpacity = 0.65,  
                   highlightOptions = highlightOptions(weight = 1, color = "coral", fillOpacity = 0.85, dashArray = "", bringToFront = TRUE),  
                   label = ~labels()) %>% 
       addLegend(pal = pal, values = rus@data[[input$value]], opacity = 0.7, position = "bottomright", title = input$value)
   })
-  
 }
-
 
 shinyApp(ui, server)
 
-?addPolygons
-
-rus@data$salary
-rus@data["salary"]
-
-
-?leafletProxy
